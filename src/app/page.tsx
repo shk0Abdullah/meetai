3693e2b726d703d33542b59f5f00beb92e441209
Abdullah Amjad <shkabdullah268@gmail.com>
2025-09-20 20:11:12 -0700

    Using shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 export default function Home() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <>
-      <Button>Click me</Button>
+      <div className="p-4 flex flex-col gap-y-4">
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          value={password}
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button className="text-white">Create User</Button>
+      </div>
     </>
   );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/app/db/index"; // your drizzle instance
+
+export const auth = betterAuth({
+    emailAndPassword:{
+        enabled: true
+    },
+    database: drizzleAdapter(db, {
+       
+        provider: "pg", 
+    }),
+});
