92c62acd668f450f593c239b2036ff1313d7ab40
Abdullah Amjad <shkabdullah268@gmail.com>
2025-09-21 00:44:15 -0700

    Adding a dirty branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,104 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 export default function Home() {
+  const { data: session, isPending, error, refetch } = authClient.useSession();
+
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-
+  if (session) {
+    return (
+      <>
+        <div className="flex flex-col gap-y-4 p-5">
+          <p className="font-bold text-2xl mx-auto">
+            Logged in as {session.user.name}
+          </p>
+          <Button
+            onClick={() => {
+              authClient.signOut();
+            }}
+          >
+            Sign Out
+          </Button>
+        </div>
+      </>
+    );
+  }
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
+  const onLogin = () => {
+    authClient.signIn.email(
+      {
+        email,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Something went wrong");
+        },
+        onSuccess: () => {
+          window.alert("Success");
+        },
+      }
+    );
+  };
   return (
     <>
-      <div className="p-4 flex flex-col gap-y-4">
-        <Input
-          placeholder="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="password"
-          value={password}
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button className="text-white">Create User</Button>
+      <div className="gap-y-5">
+        <div className="p-4 flex flex-col gap-y-4">
+          <Input
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button className="text-white" onClick={onSubmit}>
+            Create User
+          </Button>
+        </div>
+        <div className="p-4 flex flex-col gap-y-4">
+          <Input
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="password"
+            value={password}
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button className="text-white" onClick={onLogin}>
+            Login User
+          </Button>
+        </div>
       </div>
     </>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/app/db/index"; // your drizzle instance
-
+import * as schema from "@/app/db/schema";
 export const auth = betterAuth({
     emailAndPassword:{
         enabled: true
@@ -9,5 +9,8 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
        
         provider: "pg", 
+        schema:{
+            ...schema
+        },
     }),
 });
