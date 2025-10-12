7853a2426893c8da2b746753bb1226110ad7c370
Abdullah Amjad <shkabdullah268@gmail.com>
2025-10-12 01:37:17 -0700

    Making Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import { auth } from "@/lib/auth";
 // This is a server component
 export default async function Page() {
   // Get the session on the server
-  const session = await auth.api.getSession({
-    headers: headers(),
+  const session = auth.api.getSession({
+    headers: await headers(),
   });
 
   if (!session) {
